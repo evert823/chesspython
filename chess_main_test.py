28b4549077c6c633081c5024adc429f71aa86ac2
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2024-09-25 22:06:11 +0200

    better names
    better names

--- chess_main_test.py
+++ chess_main_test.py
@@ -45,8 +45,10 @@ def FEN2Json(pchessgame, pgamefilename, ppositionfilename, pfen):
 mylocalpath = "C:\\Users\\Evert Jan\\pythonprojects\\chesspython_nogithub"
 mychessgame = chessgame(mylocalpath)
 
-SwapPosition(mychessgame, "maingame", "fromfen")
-SwapPosition(mychessgame, "maingame", "mate_7_white_talkchesscom")
+SwapPosition(mychessgame, "maingame", "mate_in_3_for_white_BN_01")
+SwapPosition(mychessgame, "maingame", "mate_in_3_for_white_BN_02")
+SwapPosition(mychessgame, "maingame", "mate_in_3_for_white_BN_03")
+SwapPosition(mychessgame, "maingame", "mate_in_3_for_white_BN_04")
 #Json2FEN(mychessgame, "maingame", "08A_stalemate_2_white")
 #Json2FEN(mychessgame, "maingame", "pf_comparison_black")
 #FEN2Json(mychessgame, "maingame", "testposition", "2k5/3p4/2p1pB2/N7/2K3B1/4N3/3R4/8 w - - 0 3")
